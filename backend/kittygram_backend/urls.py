48e91299cb026a8dd044c47e62abe353cd485a4c
Антанский Буревестник <yourb1937@gmail.com>
2026-03-23 18:45:52 +0400

    nofinalkitty3_1

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,12 +4,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("OK")
+
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
