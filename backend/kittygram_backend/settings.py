25a810a9a7cc45b3123467204d8cbfdacb9bb0a6
Антанский Буревестник <yourb1937@gmail.com>
2026-03-23 18:28:59 +0400

    nofinalkitty3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 if SECRET_KEY is None:
     SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-# print('SECRET_KEY is:', os.getenv('SECRET_KEY'))
+
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
     "kittygram-final-qwfo.onrender.com"
