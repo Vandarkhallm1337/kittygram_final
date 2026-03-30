c463b9d151a0249b213c19c35bd4bcf57cd52d25
Антанский Буревестник <yourb1937@gmail.com>
2026-03-30 16:12:42 +0400

    nofinalkitty3_78

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -17,11 +17,6 @@ urlpatterns = [
     path('api/', include('djoser.urls')),  # Работа с пользователями
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
-urlpatterns += [
-    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
-    static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT),
-]
 
 if settings.DEBUG:
     urlpatterns += [static(settings.MEDIA_URL,
