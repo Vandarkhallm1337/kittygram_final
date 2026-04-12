e324305d4f5846548dabce7244b0e0fceb9a1e15
Антанский Буревестник <yourb1937@gmail.com>
2026-04-12 20:45:14 +0400

    nofinalkitty3_721

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'kittygram_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'static']
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
