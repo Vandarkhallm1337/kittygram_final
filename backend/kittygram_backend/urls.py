91ac2e79a1c63d50016e00bfdab5bb7c67c9ce6e
Антанский Буревестник <yourb1937@gmail.com>
2026-04-12 23:12:59 +0400

    nofinalkitty3_724

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
@@ -19,3 +21,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    ]
